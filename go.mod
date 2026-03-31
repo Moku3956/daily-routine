2b6dfa884f75db6fcfe8e1abdd6f240bf0685f63
Moku3956 <moku3956@icloud.com>
2026-03-31 14:35:02 +0900

    refactor: adjust domain's files

---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/Moku3956/daily-routine
+
+go 1.25.8
--- internal/domain/habit.go
+++ internal/domain/habit.go
@@ -1,1 +1,19 @@
-package adapter
+package domain
+
+type Habit struct {
+	HabitId   int
+	UserId    string
+	HabitName string
+	Category  string
+	Value     int
+	Unit      string
+	PCost     int
+	MCost     int
+	Must      bool
+}
+
+type HabitRepository interface {
+	Save(habit *Habit) error
+	Update(habit *Habit) error
+	Delete(id int) error
+}
